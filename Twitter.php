2f9e3b435dde67908f32209a7d1cd24aceaa1bd3
RichWeber <rbagatyi@gmail.com>
2015-02-11 08:56:31 +0200

    updated

--- Twitter.php
+++ Twitter.php
@@ -1,56 +1,56 @@
 <?php
-class YiiTwitter extends CApplicationComponent
+/**
+ * Yii2 extension to the Twitter API
+ *
+ * @copyright Copyright &copy; Roman Bahatyi, richweber.net, 2015
+ * @package yii2-twitter
+ * @version 1.0.0
+ */
+
+namespace richweber\twitter;
+
+class Twitter
 {
-	//The Twitter Apps key, set in config.
-	public $consumer_key = '';
-	
-	//The Twitter Apps secret key, set in config.	
-	public $consumer_secret = '';
-	
-	//The call back url for twitter
-	public $callback = '';	
-	
-	//Can be set in config to not load OAuth.php
-	public $load_oauth = true;	
-	
-	//Have we loaded our dependencies
-	private static $registeredScripts = false;
-	public function init() {
-		$this->registerScripts();
-		parent::init();	
-	}	
-	
-	/**
-	* Returns the callback url set in config
-	*/
-	public function getCallback() {
-		return $this->callback;
-	}
-	
-	
-	/**
-	* Use this one for when we need to authicate oursevles with twitter
-	*/
-	public function getTwitter() {
-		return new TwitterOAuth($this->consumer_key,$this->consumer_secret);			
-	}
-	/**
-	* Use this for after we have a token and a secret for the use.
-	*	(you must save these in order for them to be usefull
-	*/
-	public function getTwitterTokened($token,$secret) {
-		return new TwitterOAuth($this->consumer_key,$this->consumer_secret,$token,$secret);	
-	}
-		
     /**
-    * Registers twitteroauth.php & OAuth.php
-    */
-    public function registerScripts() {
-    	if (self::$registeredScripts) return;
-    	self::$registeredScripts = true;
-		if($this->load_oauth)
-			require dirname(__FILE__).'/OAuth.php';
-		require dirname(__FILE__).'/twitteroauth.php';
-	}	
-		
+     * The Twitter Apps key, set in config
+     * @var string
+     */
+    public $consumer_key = '';
+
+    /**
+     * The Twitter Apps secret key, set in config
+     * @var string
+     */
+    public $consumer_secret = '';
+
+    /**
+     * The call back url for twitter
+     * @var string
+     */
+    public $callback = '';
+
+    /**
+     * Returns the callback url set in config
+     */
+    public function getCallback()
+    {
+        return $this->callback;
+    }
+
+    /**
+     * Use this one for when we need to authicate oursevles with twitter
+     */
+    public function getTwitter()
+    {
+        return new TwitterOAuth($this->consumer_key,$this->consumer_secret);
+    }
+
+    /**
+     * Use this for after we have a token and a secret for the use.
+     *    (you must save these in order for them to be usefull
+     */
+    public function getTwitterTokened($token,$secret)
+    {
+        return new TwitterOAuth($this->consumer_key,$this->consumer_secret,$token,$secret);
+    }
 }
